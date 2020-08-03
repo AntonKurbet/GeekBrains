a74bf093f7558e737559fd2b76faf2b0f48fb0ac
AntonKurbet <anton.kurbet@gmail.com>
2020-08-03 19:55:44 +0300

    task 3

--- ru.geekbrains.java1.lesson3/src/TicTacToe.java
+++ ru.geekbrains.java1.lesson3/src/TicTacToe.java
@@ -3,6 +3,11 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
+    // 1. Полностью разобраться с кодом;
+    // 2. Переделать проверку победы, чтобы она не была реализована просто набором условий.
+    // 3. * Попробовать переписать логику проверки победы, чтобы она работала для поля 5х7 и количества фишек 4.
+    // 4. *** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока, и пытаться выиграть сам.
+
     private static final char DOT_HUMAN = 'X';
     private static final char DOT_AI = 'O';
     private static final char DOT_EMPTY = '.';
@@ -10,11 +15,17 @@ public class TicTacToe {
     private static final Random RANDOM = new Random();
     private static int fieldSizeY;
     private static int fieldSizeX;
+    private static int dotNumber;
+    private static int lastX;
+    private static int lastY;
     private static char[][] field;
 
     private static void initField() {
-        fieldSizeX = 3;
-        fieldSizeY = 3;
+        //fieldSizeX = 3;
+        //fieldSizeY = 3;
+        fieldSizeX = 5;
+        fieldSizeY = 7;
+        dotNumber = 4;
         field = new char[fieldSizeY][fieldSizeX];
         for (int y = 0; y < fieldSizeY; y++) {
             for (int x = 0; x < fieldSizeX; x++) {
@@ -42,14 +53,13 @@ public class TicTacToe {
     }
 
     private static void humanTurn() {
-        int x;
-        int y;
         do {
-            System.out.println("Введите координаты хода X и Y (от 1 до 3) через пробел >>> ");
-            x = SCANNER.nextInt() - 1;
-            y = SCANNER.nextInt() - 1;
-        } while (!isValidCell(x, y) || !isEmptyCell(x, y));
-        field[y][x] = DOT_HUMAN;
+            System.out.printf("Введите координаты хода X (от 1 до %d) и Y (от 1 до %d) через пробел >>> \n",
+                              fieldSizeX,fieldSizeY);
+            lastX = SCANNER.nextInt() - 1;
+            lastY = SCANNER.nextInt() - 1;
+        } while (!isValidCell(lastX, lastY) || !isEmptyCell(lastX, lastY));
+        field[lastY][lastX] = DOT_HUMAN;
     }
 
     private static boolean isEmptyCell(int x, int y) {
@@ -61,13 +71,11 @@ public class TicTacToe {
     }
 
     private static void aiTurn() {
-        int x;
-        int y;
         do {
-            x = RANDOM.nextInt(fieldSizeX);
-            y = RANDOM.nextInt(fieldSizeY);
-        } while (!isEmptyCell(x, y));
-        field[y][x] = DOT_AI;
+            lastX = RANDOM.nextInt(fieldSizeX);
+            lastY = RANDOM.nextInt(fieldSizeY);
+        } while (!isEmptyCell(lastX, lastY));
+        field[lastY][lastX] = DOT_AI;
     }
 
     private static boolean checkDraw() {
@@ -79,29 +87,111 @@ public class TicTacToe {
         return true;
     }
 
-    private static boolean checkWin(char c) {
-        // hor
-        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
-        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
-        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+    // задача 2
+    private static boolean checkWin2(char c) {
+        return checkHor2(c) | checkVer2(c) | checkDia2(c);
+    }
+    // для квадратного поля с количеством фишек равным его размеру
+    private static boolean checkDia2(char c) {
+        boolean result1 = true;
+        boolean result2 = true;
+        for (int x = 0; x < fieldSizeX; x++) {
+            result1 = result1 && field[x][x] == c;
+            result2 = result2 && field[x][fieldSizeX - x - 1] == c;
+        }
+        return result1 | result2;
+    }
+    private static boolean checkVer2(char c) {
+        boolean result = false;
+        for (int x = 0; x < fieldSizeX; x++) {
+            result = true;
+            for (int y = 0; y < fieldSizeY; y++) {
+                result = result && field[y][x] == c;
+            }
+            if (result) break;
+        }
+        return result;
+    }
+    private static boolean checkHor2(char c) {
+        boolean result = false;
+        for (int y = 0; y < fieldSizeY; y++) {
+            result = true;
+            for (int x = 0; x < fieldSizeX; x++) {
+                result = result && field[y][x] == c;
+            }
+            if (result) break;
+        }
+        return result;
+    }
 
-        // ver
-        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
-        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
-        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
+    // задача 3
+    private static boolean checkWin3(char c) {
+        return checkHorline3(c) | checkVerLine3(c) | checkDiaLine3(c);
+    }
+    private static boolean checkHorline3(char c) {
+        int sum = 0;
+        for (int i = 0; i < fieldSizeX - dotNumber; i ++) {
+            sum = 0;
+            for (int x = 0; x < dotNumber; x++) {
+                if (field[lastY][x + i] == c) sum = sum +  1;
+                else sum = 0;
+                if (sum == dotNumber) break;
+            }
+            if (sum == dotNumber) break;
+        }
+        return sum == dotNumber;
+    }
+    private static boolean checkVerLine3(char c) {
+        int sum = 0;
+        for (int i = 0; i < fieldSizeY - dotNumber; i ++) {
+            sum = 0;
+            for (int y = 0; y < dotNumber; y++) {
+                if (field[y + i][lastX] == c) sum = sum +  1;
+                else sum = 0;
+                if (sum == dotNumber) break;
+            }
+            if (sum == dotNumber) break;
+        }
+        return sum == dotNumber;
+    }
+    private static int diaFunc1 (int x) {
+        // equation of diagonal left to right
+        return x - lastX + lastY;
+    }
+    private static int diaFunc2 (int x) {
+        // equation of diagonal right to left
+        return -x + lastX + lastY;
+    }
+    private static boolean checkDiaLine3(char c) {
+        int sum = 0;
+        for (int x = 0; x<fieldSizeX; x++) {
+            int y = diaFunc1(x);
+            if (y >= 0 && y < fieldSizeY) {
+                if (field[y][x] == c) sum = sum + 1;
+                else sum = 0;
+                if (sum == dotNumber) break;
+            }
+        }
 
-        // dia
-        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
-        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
-        return false;
-
+        if (sum != dotNumber) {
+            sum = 0;
+            for (int x = fieldSizeX - 1; x >= 0; x--) {
+                int y = diaFunc2(x);
+                if (y >= 0 && y < fieldSizeY) {
+                    if (field[y][x] == c) sum = sum + 1;
+                    else sum = 0;
+                    if (sum == dotNumber) break;
+                }
+            }
+        }
+        return sum == dotNumber;
     }
 
     public static void main(String[] args) {
-        initField();
-        printField();
         String answer;
         do {
+            initField();
+            printField();
             while (true) {
                 humanTurn();
                 if (checkEndGame(DOT_HUMAN, "Human win!")) break;
@@ -116,7 +206,8 @@ public class TicTacToe {
 
     private static boolean checkEndGame(char dot, String winMessage) {
         printField();
-        if (checkWin(dot)) {
+        //if (checkWin2(dot)) {
+        if (checkWin3(dot)) {
             System.out.println(winMessage);
             return true;
         }
