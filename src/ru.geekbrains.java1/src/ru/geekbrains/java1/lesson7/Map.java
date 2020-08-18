7812e30139062d73c5fd5827e7e4ae10ad37bf68
AntonKurbet <anton.kurbet@gmail.com>
2020-08-18 06:57:51 +0300

    xo gui

---
+++ src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson7/Map.java
@@ -0,0 +1,34 @@
+package ru.geekbrains.java1.lesson7;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Map extends JPanel {
+    public static final int MODE_HVH = 0;
+    public static final int MODE_HVA = 1;
+
+    private JButton[][] field;
+
+    Map() {
+        setBackground(Color.DARK_GRAY);
+    }
+
+    private void initField(int fieldSizeX, int fieldSizeY) {
+        field = new JButton[fieldSizeY][fieldSizeX];
+
+        setLayout(new GridLayout(fieldSizeX, fieldSizeY));
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = new JButton();
+                field[y][x].setBackground(Color.LIGHT_GRAY);
+                this.add(field[y][x]);
+            }
+        }
+
+        this.revalidate();
+    }
+
+    void startNewGame(int mode, int fieldSizeX, int fieldSizeY, int winLength) {
+        initField(fieldSizeX,fieldSizeY);
+    }
+}
