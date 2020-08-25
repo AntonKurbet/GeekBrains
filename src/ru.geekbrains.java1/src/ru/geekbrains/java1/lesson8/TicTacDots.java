6028ee8589b5ee877e0200d884c9964aa65a14e4
Anton A. Kurbet <AKurbet@bis.ru>
2020-08-25 16:34:42 +0300

    moves + png

--- src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson8/TicTacDots.java
+++
@@ -1,5 +0,0 @@
-package ru.geekbrains.java1.lesson8;
-
-public enum TicTacDots {
-    DOT_HUMAN,DOT_AI,DOT_EMPTY
-}
--- src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson8/XoGUI.java
+++
@@ -1,7 +0,0 @@
-package ru.geekbrains.java1.lesson8;
-
-public class XoGUI {
-    public static void main(String[] args) {
-        new GameWindow();
-    }
-}
