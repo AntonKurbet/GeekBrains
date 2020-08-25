155866ffe333c8b759de1f15e4dab2abf3795cf0
Anton A. Kurbet <AKurbet@bis.ru>
2020-08-25 12:52:11 +0300

    work1

--- src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson8/Map.java
+++ src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson8/Map.java
@@ -1,9 +1,12 @@
 package ru.geekbrains.java1.lesson8;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
+import java.io.IOException;
 
 public class Map extends JPanel {
     public static final int MODE_HVH = 0;
@@ -17,9 +20,19 @@ public class Map extends JPanel {
     private static int mapHeight;
     private static int cellSizeX;
     private static int cellSizeY;
+    private static final int FONT_SIZE = 60;
+    private static final Color backColor = Color.LIGHT_GRAY;
+    private Image imgHuman;
+    private Image imgAI;
 
     Map() {
-        setBackground(Color.LIGHT_GRAY);
+        setBackground(backColor);
+        try {
+            imgHuman = ImageIO.read(new File("./src/img/humanDot.png"));
+            imgAI = ImageIO.read(new File("./src/img/aiDot.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         addMouseListener(new MouseAdapter() {
             @Override
             public void mouseReleased(MouseEvent e) {
@@ -51,13 +64,11 @@ public class Map extends JPanel {
         gameState = TicTacToe.checkEndGame(TicTacDots.DOT_AI);
         if (gameState == GameStates.STATE_WIN_AI || gameState == GameStates.STATE_DRAW) {
             repaint();
-            return;
         }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
-
         super.paintComponent(g);
         render(g);
     }
@@ -90,7 +101,18 @@ public class Map extends JPanel {
                 message = "Shit Happens!";
         }
 
-        g.drawString(message, DOT_PADDING, mapHeight / 2);
+        g.fill3DRect(DOT_PADDING,(int) (mapHeight * 0.35),
+                     mapWidth - 2 * DOT_PADDING, (int) (mapHeight * 0.3), true  );
+
+        g.setColor(Color.WHITE);
+
+        Font font = new Font("Impact", Font.PLAIN, FONT_SIZE);
+        g.setFont(font);
+
+        FontMetrics metrics = g.getFontMetrics(font);
+        int width = metrics.stringWidth(message);
+
+        g.drawString(message, (mapWidth - width) / 2, (mapHeight + FONT_SIZE) /  2 );
     }
 
     private void paintMoves(Graphics g) {
@@ -98,15 +120,15 @@ public class Map extends JPanel {
             for (int y = 0; y < fieldSizeY; y++) {
                 switch (TicTacToe.getFieldCell(x,y)) {
                     case DOT_HUMAN:
-                        g.drawLine(x * cellSizeX + DOT_PADDING, y * cellSizeY + DOT_PADDING,
-                                (x + 1) * cellSizeX - DOT_PADDING, (y + 1) * cellSizeY - DOT_PADDING);
-                        g.drawLine(x * cellSizeX + DOT_PADDING, (y + 1) * cellSizeY - DOT_PADDING,
-                                (x + 1) * cellSizeX - DOT_PADDING, y * cellSizeY + DOT_PADDING);
+                        g.drawImage(imgHuman, x * cellSizeX + DOT_PADDING, y * cellSizeY + DOT_PADDING,
+                                cellSizeX - DOT_PADDING * 2, cellSizeY - DOT_PADDING * 2,
+                                backColor, null);
 
                         break;
                     case DOT_AI:
-                        g.drawOval(x * cellSizeX + DOT_PADDING, y * cellSizeY + DOT_PADDING,
-                                cellSizeX - 2 * DOT_PADDING, cellSizeY - 2 * DOT_PADDING);
+                        g.drawImage(imgAI, x * cellSizeX + DOT_PADDING, y * cellSizeY + DOT_PADDING,
+                                cellSizeX - DOT_PADDING * 2, cellSizeY - DOT_PADDING * 2,
+                                backColor, null);
                         break;
                 }
             }
--- src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson8/XoGUI.java
+++ src/ru.geekbrains.java1/src/ru/geekbrains/java1/lesson8/XoGUI.java
@@ -1,8 +1,5 @@
 package ru.geekbrains.java1.lesson8;
 
-import javax.swing.*;
-import java.awt.*;
-
 public class XoGUI {
     public static void main(String[] args) {
         new GameWindow();
